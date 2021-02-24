aa051ac0f0c4c2ead572fc77f1b09d572fcbd0f3
Andrew M. Silva <andrewsaxx@gmail.com>
2021-02-24 13:44:43 -0300

    feat: connecting to database

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+import { LocalDB } from './connection/local-db';
 import { createServer } from 'http';
 import { App } from './app';
 
+// Database connection
+LocalDB.then(()=>console.log('🛰  Database connected'))
+
+// Server listening
 const httpServer = createServer(App);
-httpServer.listen({ port: 3000 }, () => console.log(`\n🚀 GraphQL is running on http://localhost:3000/graphql\n`));
+httpServer.listen({ port: 3000 }, () => console.log(`\n🚀 GraphQL is running on http://localhost:3000/graphql`));

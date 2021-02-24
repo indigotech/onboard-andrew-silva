9de032c48ac6d14ec3fa3e8aa25269ca3604edfb
Andrew M. Silva <andrewsaxx@gmail.com>
2021-02-24 14:32:46 -0300

    style: changing log icons

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { createServer } from 'http';
 import { App } from './app';
 
 // Database connection
-LocalDB.then(()=>console.log('🛰  Database connected'))
+LocalDB.then(() => console.log('🟢 Database connected')).catch(() => console.log('🔴 Database not connected!'));
 
 // Server listening
 const httpServer = createServer(App);
-httpServer.listen({ port: 3000 }, () => console.log(`\n🚀 GraphQL is running on http://localhost:3000/graphql`));
+httpServer.listen({ port: 3000 }, () => console.log(`🟢 GraphQL is running on http://localhost:3000/graphql`));

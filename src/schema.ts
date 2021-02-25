0ce9472d0f5e2c7199a97b259bdbb929ea2f3e49
Andrew M. Silva <andrewsaxx@gmail.com>
2021-02-25 11:21:45 -0300

    feat: initializing user resolver

--- src/schema.ts
+++ src/schema.ts
@@ -2,8 +2,9 @@ import 'graphql-import-node';
 import { GraphQLSchema } from 'graphql';
 import { buildSchemaSync } from 'type-graphql';
 import { HelloResolver } from './resolver/hello-resolver';
+import { UserResolver } from './resolver/user-resolver';
 
 // Load resolvers
 export const Schema: GraphQLSchema = buildSchemaSync({
-  resolvers: [HelloResolver],
+  resolvers: [HelloResolver, UserResolver],
 });

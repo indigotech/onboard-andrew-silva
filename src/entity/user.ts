31991de0cb7cc37f826e8e4a4dd12270938bab71
Andrew M. Silva <andrewsaxx@gmail.com>
2021-02-25 12:00:31 -0300

    feat: adding create user mutation and input

--- src/entity/user.ts
+++ src/entity/user.ts
@@ -1,28 +1,38 @@
-import { Entity, PrimaryGeneratedColumn, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Entity, BaseEntity, PrimaryGeneratedColumn, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { ObjectType, Field, ID } from "type-graphql";
 
 @Entity()
-export class User {
+@ObjectType()
+export class User extends BaseEntity {
+  @Field(() => ID)
   @PrimaryGeneratedColumn()
   id!: number;
 
+  @Field(() => String)
   @Column()
   name!: string;
 
+  @Field(() => String)
   @PrimaryColumn()
   email!: string;
 
+  @Field(() => String)
   @Column()
   password!: string;
 
+  @Field(() => Date)
   @Column({ type: 'date' })
   birthDate!: Date;
 
-  @Column()
+  @Field(() => Boolean)
+  @Column({ default: true })
   isActive!: boolean;
 
+  @Field(() => Date)
   @CreateDateColumn({ type: 'timestamp' })
   creationDate!: Date;
 
+  @Field(() => Date)
   @UpdateDateColumn({ type: 'timestamp' })
   updateDate!: Date;
 }
--- src/resolver/user-resolver.ts
+++ src/resolver/user-resolver.ts
@@ -1,9 +1,17 @@
-import { Resolver, Query } from "type-graphql";
+import { Resolver, Query, Mutation, Arg } from 'type-graphql';
+import { User } from '../entity/user';
+import { CreateUserInput } from '../input/create-user-input';
 
 @Resolver()
 export class UserResolver {
   @Query(() => String)
   users() {
     return 'Users';
+  }
+
+  @Mutation(() => User)
+  async createUser(@Arg('data') data: CreateUserInput) {
+    const user = User.create(data);
+    return user;
   }
 }

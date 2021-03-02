fceacc31a0443379b8060ff1ddaffdd49c1d79da
Andrew M. Silva <andrewsaxx@gmail.com>
2021-03-02 10:51:16 -0300

    feat: testing users fetching

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,5 +1,23 @@
-  describe('#hello()', () => {
-    it('should print a message', () => {
-      console.log('Hello, test!');
-    });
+import supertest from 'supertest';
+
+const url = `http://localhost:3000`;
+const request = supertest(url);
+
+describe('GraphQL Tests', () => {
+  it('Fetching users', (done) => {
+    request
+      .post('/graphql')
+      .send({
+        query: '{ users { id, name, email, birthDate } }',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        done();
+      });
   });
+});

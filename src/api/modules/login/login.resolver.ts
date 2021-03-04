7669918d15b0843e769b79386fbb25b339a8a9ba
Andrew M. Silva <andrewsaxx@gmail.com>
2021-03-04 17:40:24 -0300

    feat: authenticating user

--- src/api/modules/login/login.resolver.ts
+++ src/api/modules/login/login.resolver.ts
@@ -1,19 +1,19 @@
 import { Resolver, Mutation, Arg } from 'type-graphql';
+import { LoginInput } from './login.input';
 import { LoginType } from './login.type';
+import { UserEntity } from '@data/entity/user.entity';
+
 import bcrypt from 'bcrypt';
 
 @Resolver()
 export class LoginResolver {
   @Mutation(() => LoginType)
-  async login() {
-    return {
-      user: {
-        id: 12,
-        name: 'User Name',
-        email: 'User e-mail',
-        birthDate: new Date(),
-      },
-      token: 'the_token',
-    };
+  async login(@Arg('data') data: LoginInput) {
+    const user: UserEntity | undefined = await UserEntity.findOne({ email: data.email });
+    if (user) {
+      if (await bcrypt.compare(data.password, user.password)) {
+        return { user, token: 'the_token' };
+      }
+    }
   }
 }

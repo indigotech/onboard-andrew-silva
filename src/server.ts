d9e5f987c9e677ca31de6eb75cb3e068cc3c0720
Andrew M. Silva <andrewsaxx@gmail.com>
2021-03-04 12:09:26 -0300

    feat: returnig argument errors

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,10 @@ import cors from 'cors';
 import { Schema } from '@api/schema';
 import { createServer, Server as HttpServer } from 'http';
 import dotenv from 'dotenv';
+import { ArgumentValidationError } from 'type-graphql';
 
 // Config environments
-dotenv.config({ path: (process.env.TEST == 'true' ? '.test.env' : '.env') });
+dotenv.config({ path: process.env.TEST == 'true' ? '.test.env' : '.env' });
 
 export const Server = async (): Promise<HttpServer> => {
   // Create Apollo Server
@@ -17,10 +18,32 @@ export const Server = async (): Promise<HttpServer> => {
     schema: Schema,
     validationRules: [depthLimit(7)],
     formatError: (err) => {
+      const originalError = err.originalError;
+      let code: number = 400;
+      let message: string;
+      let detail;
+
+      if (originalError instanceof ArgumentValidationError) {
+        const errors = originalError.validationErrors.map((validationError) => {
+          if (validationError.constraints) {
+            const messages = [];
+            for (let key in validationError.constraints) {
+              messages.push(validationError.constraints[key]);
+            }
+            return messages;
+          }
+        });
+        message = 'Argumentos inválidos';
+        detail = errors;
+      } else {
+        message = err.message;
+        detail = err?.extensions?.exception.detail;
+      }
+
       return {
-        code: err?.extensions?.code,
-        message: err.message,
-        detail: err?.extensions?.exception.detail,
+        code: code,
+        message: message,
+        detail: detail,
       };
     },
   });

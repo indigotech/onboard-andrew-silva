11f260304a78b281e91c7874c929f2cd472555f4
Andrew M. Silva <andrewsaxx@gmail.com>
2021-03-05 09:27:13 -0300

    feat: adding errors

--- src/api/modules/login/login.resolver.ts
+++ src/api/modules/login/login.resolver.ts
@@ -1,4 +1,5 @@
 import { Resolver, Mutation, Arg } from 'type-graphql';
+import { BaseError } from '@api/error/base-error';
 import { LoginInput } from './login.input';
 import { LoginType } from './login.type';
 import { UserEntity } from '@data/entity/user.entity';
@@ -10,9 +11,14 @@ export class LoginResolver {
   @Mutation(() => LoginType)
   async login(@Arg('data') data: LoginInput) {
     const user: UserEntity | undefined = await UserEntity.findOne({ email: data.email });
-    if (user && (await bcrypt.compare(data.password, user.password))) {
-      return { user, token: 'the_token' };
+    if (user) {
+      if (await bcrypt.compare(data.password, user.password)) {
+        return { user, token: 'the_token' };
+      } else {
+        throw new BaseError(401, 'Email ou senha incorretos');
+      }
+    } else {
+      throw new BaseError(404, 'Email não cadastrado');
     }
-    throw new Error('Email ou senha incorretos');
   }
 }

40659422b264d5fe328b692fda7bcc6014a35a04
Andrew M. Silva <andrewsaxx@gmail.com>
2021-03-08 13:01:47 -0300

    feat: testing login

--- src/test/index.ts
+++ src/test/index.ts
@@ -36,6 +36,88 @@ describe('GraphQL: Hello query', () => {
   });
 });
 
+describe('GraphQL: Login', () => {
+  const mutation = `
+  mutation login($data: LoginInput!) {
+    login(data: $data) {
+      user {
+        id
+        name
+        email
+        birthDate
+      }
+      token
+    }
+  }`;
+
+  it('should login successfully', async () => {
+    const user = UserEntity.create({
+      name: 'Luke Skywalker',
+      email: 'skylwalker.top@gmail.com',
+      password: 'a1ÊÇ7ma2',
+      birthDate: new Date(),
+    });
+    await user.save();
+
+    const input: LoginInput = {
+      email: user.email,
+      password: 'a1ÊÇ7ma2',
+      rememberMe: false,
+    };
+
+    const res = await createRequest(mutation, { data: input });
+
+    expect(res.body).to.not.own.property('errors');
+    expect(res.body.data.login.token).to.be.an('string');
+    expect(res.body.data.login.user).to.include({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      birthDate: user.birthDate.toISOString(),
+    });
+  });
+
+  it('should trigger unknown user error', async () => {
+    const input: LoginInput = {
+      email: 'skylwalker.top@gmail.com',
+      password: 'a1ÊÇ7ma2',
+      rememberMe: false,
+    };
+
+    const res = await createRequest(mutation, { data: input });
+
+    expect(res.body.data).to.be.null;
+    expect(res.body).to.own.property('errors');
+
+    const errorMessages = res.body.errors.map((error: { message: string }) => error.message);
+    expect(errorMessages).to.include('Email não cadastrado');
+  });
+
+  it('should trigger invalid password error', async () => {
+    const user = UserEntity.create({
+      name: 'Luke Skywalker',
+      email: 'skylwalker.top@gmail.com',
+      password: 'a1ÊÇ7ma2',
+      birthDate: new Date(),
+    });
+    await user.save();
+
+    const input: LoginInput = {
+      email: user.email,
+      password: 'a1ÊÇ7ma',
+      rememberMe: false,
+    };
+
+    const res = await createRequest(mutation, { data: input });
+
+    expect(res.body.data).to.be.null;
+    expect(res.body).to.own.property('errors');
+
+    const errorMessages = res.body.errors.map((error: { message: string }) => error.message);
+    expect(errorMessages).to.include('Email ou senha incorretos');
+  });
+});
+
 describe('GraphQL: User - createUser', () => {
   const mutation = `
     mutation createUser($data: UserInput!) {

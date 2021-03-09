a3f64d6afce4d9b9ad0caa25568b125282e99d5f
Andrew M. Silva <andrewsaxx@gmail.com>
2021-03-09 15:16:59 -0300

    feat: creating seed for users

---
+++ src/data/seed/user.seed.ts
@@ -0,0 +1,18 @@
+import { UserEntity } from '@data/entity/user.entity';
+import faker from 'faker';
+
+export const UserSeed = async () => {
+  faker.seed(10);
+
+  await UserEntity.clear();
+
+  for (let i = 0; i < 50; i++) {
+    const user = UserEntity.create({
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: '123abcd',
+      birthDate: faker.date.past(80),
+    });
+    await user.save();
+  }
+};

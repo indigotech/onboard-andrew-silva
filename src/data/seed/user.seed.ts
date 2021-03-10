9546461ce6029e73f25b582852316cf3031f080e
Andrew M. Silva <andrewsaxx@gmail.com>
2021-03-10 10:49:26 -0300

    feat: adding option to seed a given number os users

--- src/data/seed/user.seed.ts
+++ src/data/seed/user.seed.ts
@@ -1,12 +1,12 @@
 import { UserEntity } from '@data/entity/user.entity';
 import faker from 'faker';
 
-export const UserSeed = async () => {
+export const UserSeed = async (num: Number = 50) => {
   faker.seed(10);
 
   await UserEntity.clear();
 
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < num; i++) {
     const user = UserEntity.create({
       name: faker.name.findName(),
       email: faker.internet.email(),

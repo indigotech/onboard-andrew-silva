a896980a0f9aa62392642ac024d55a9dc605aa3d
Andrew M. Silva <andrewsaxx@gmail.com>
2021-03-10 17:07:59 -0300

    feat: adding page data tu users instead of extend class

--- src/api/schema/user/users.type.ts
+++ src/api/schema/user/users.type.ts
@@ -1,9 +1,12 @@
-import { ObjectType, Field, ID } from 'type-graphql';
+import { ObjectType, Field, Int } from 'type-graphql';
 import { PageType } from '@api/schema/pagination/page.type';
 import { UserType } from './user.type';
 
 @ObjectType()
-export class UsersType extends PageType {
+export class UsersType {
   @Field(() => [UserType], { description: 'List of users' })
   users!: [UserType];
+
+  @Field(() => PageType, { description: 'Page data' })
+  page!: PageType;
 }

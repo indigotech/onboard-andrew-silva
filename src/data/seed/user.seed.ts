58ad8f36f0805f37e9803a0d23395f1d4c878f0b
Andrew M. Silva <andrewsaxx@gmail.com>
2021-03-11 17:01:08 -0300

    feat: returning seeded users

--- src/data/seed/user.seed.ts
+++ src/data/seed/user.seed.ts
@@ -1,18 +1,23 @@
 import { UserEntity } from '@data/entity/user.entity';
 import faker from 'faker';
 
-export const UserSeed = async (num: Number = 50) => {
+export const UserSeed = async (num: Number = 50): Promise<UserEntity[]> => {
   faker.seed(10);
 
   await UserEntity.clear();
 
+  const users = [];
   for (let i = 0; i < num; i++) {
-    const user = UserEntity.create({
-      name: faker.name.findName(),
-      email: faker.internet.email(),
-      password: '123abcd',
-      birthDate: faker.date.past(80),
-    });
-    await user.save();
+    users.push(
+      UserEntity.create({
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        password: '123abcd',
+        birthDate: faker.date.past(80),
+      }),
+    );
+    await users[i].save();
   }
+
+  return users;
 };

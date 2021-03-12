dea75b8ec2d4a79c118ed7aeae2349ee1888be9e
Andrew M. Silva <andrewsaxx@gmail.com>
2021-03-12 14:20:39 -0300

    feat: creating address entity

---
+++ src/data/entity/address.entity.ts
@@ -0,0 +1,35 @@
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { UserEntity } from './user.entity';
+
+@Entity('address')
+export class AddressEntity extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @ManyToOne(() => UserEntity, (user) => user.addresses, { onDelete: 'CASCADE' })
+  user!: UserEntity;
+
+  @Column({ nullable: true })
+  label?: string;
+
+  @Column()
+  cep!: string;
+
+  @Column()
+  street!: string;
+
+  @Column()
+  streetNumber!: number;
+
+  @Column({ nullable: true })
+  complement?: string;
+
+  @Column()
+  neighborhood!: string;
+
+  @Column()
+  city!: string;
+
+  @Column()
+  state!: string;
+}

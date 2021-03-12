a2e5b6fde40e2b33f440dcbe340a9c117ec4cbe9
Andrew M. Silva <andrewsaxx@gmail.com>
2021-03-12 10:37:14 -0300

    feat: adusting max limit and validation messages

--- src/api/schema/pagination/page.input.ts
+++ src/api/schema/pagination/page.input.ts
@@ -1,13 +1,14 @@
 import { Field, InputType, Int } from 'type-graphql';
-import { Min } from 'class-validator';
+import { Max, Min } from 'class-validator';
 
 @InputType()
 export class PageInput {
   @Field(() => Int, { description: 'Número de elementos ignorados', nullable: true })
-  @Min(0, { message: 'O número de elementos ignorados precisa ser igual ou maior que zero' })
+  @Min(0, { message: 'O número mínimo de elementos ignorados é 0' })
   offset?: number;
 
   @Field(() => Int, { description: 'Número máximo de elementos a serem retornados', defaultValue: 10 })
-  @Min(0, { message: 'O número limite precisa ser igual ou maior que zero' })
+  @Min(1, { message: 'O número limite mínimo é 1' })
+  @Max(100, { message: 'O número limite máximo é 100' })
   limit!: number;
 }
